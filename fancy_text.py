b6ba1f4e492a733db130e9fef0a002098dae2cba
UnormalDude <103203921+UnormalDude@users.noreply.github.com>
2025-05-15 11:32:35 -0400

    Update fancy_text.py
    Adding commenting to make everything more clear

--- fancy_text.py
+++ fancy_text.py
@@ -1,14 +1,15 @@
-import os
+#MADE BY JOHN CUNNINGTON
+#THIS CODE IS A CUSTOM MODULE TO ALLOW FOR EASIER CHANGING OF FORMATTING OF A PYTHON SCRIPT WHILE BEING EXTREMELY MINIMAL. NO OTHER LIBRARIES ARE NEEDED
 
 def text_edit(name):
 
-    colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white' ]
-    color_codes = ['31', '32', '33', '34', '35', '36', '37']
+    colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white' ] #List of available colors
+    color_codes = ['31', '32', '33', '34', '35', '36', '37'] #The ANSI codes that correlate
 
-    if name in colors:
-        index = colors.index(name)
-        code = color_codes[index]
-        return f"\033[{code}m"
+    if name in colors: #If the color name is available 
+        index = colors.index(name) #Get the index of the color
+        code = color_codes[index] #Get the correlated ANSI code
+        return f"\033[{code}m" #Plug it into the format here
         
     elif name == "italic":
         return "\x1B[3m"
@@ -19,10 +20,10 @@ def text_edit(name):
     elif name == "title":
         return "\033[1m\x1B[3m"
     
-    elif name == "reset":
+    elif name == "reset": #Return text to default
         return "\033[0m"
 
     #COLORS-----------------------------------------------------------------------------
 
     else:
-        return ""
+        return "" #return nothing if the arg does not exist in the code
